073b838ff7a9e7c54b9d038438d02dfb4e212a4a
G5Olivieri <glayson.murollo75@gmail.com>
2025-06-25 21:38:21 -0300

    fix: update CouchDB password and refactor product database sync logic

--- src/product/product-db.ts
+++ src/product/product-db.ts
@@ -1,9 +1,9 @@
 import { config } from "@/config/env";
-import { sync } from "@/db/sync";
+import { PouchDB } from "@/db/pouchdb-config";
 import type { Product } from "@/product/product";
-import { PouchDB } from "../db/pouchdb-config";
 
 const POUCHDB_ADAPTER = config.pouchdb.adapter || "idb";
+const REMOTE_COUCHDB_URL = config.couchdbUrl;
 
 let _productDB: PouchDB.Database<Product> | null = null;
 
@@ -24,8 +24,8 @@ export const getProductDB = async (): Promise<PouchDB.Database<Product>> => {
     index: { fields: ["category"] },
   });
 
-  sync(_productDB);
-
+  // TODO: sync stock
+  await syncProductDB(_productDB);
   return _productDB;
 };
 
@@ -34,4 +34,55 @@ export const resetProductDB = async (): Promise<void> => {
     await _productDB.destroy();
     _productDB = null;
   }
+};
+
+export const syncProductDB = async (
+  productDB: PouchDB.Database<Product>,
+): Promise<void> => {
+  const syncHandler = productDB.sync(`${config.couchdbUrl}/${productDB.name}`, {
+    live: true,
+    retry: true,
+    filter: (doc: Product) => {
+      const filteredDoc = { ...doc };
+      if ("stock" in filteredDoc) {
+        // @ts-expect-error TypeScript doesn't know about the stock field
+        delete filteredDoc.stock;
+      }
+      return true;
+    },
+  });
+
+  syncHandler.on("change", async (info) => {
+    console.log("Product DB Sync change:", info);
+    if (info.direction === "pull" && info.change.docs) {
+      console.log("Product DB Sync pull change:", info.change.docs);
+      for (const doc of info.change.docs as Product[]) {
+        const localDoc = await productDB.get<Product>(doc._id);
+        if (localDoc.stock !== doc.stock) {
+          console.log("Product DB Sync stock changed:", localDoc, doc);
+          await productDB.put({ ...localDoc, stock: doc.stock });
+        }
+      }
+    }
+  });
+
+  syncHandler.on("error", (err) => {
+    console.error("Product DB Sync error:", err);
+  });
+
+  syncHandler.on("denied", (err) => {
+    console.error("Product DB Sync denied:", err);
+  });
+
+  syncHandler.on("paused", (err) => {
+    console.warn("Product DB Sync paused:", err);
+  });
+
+  syncHandler.on("active", () => {
+    console.log("Product DB Sync active");
+  });
+
+  syncHandler.on("complete", () => {
+    console.log("Product DB Sync complete");
+  });
 };
